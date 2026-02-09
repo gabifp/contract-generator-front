cb81fb1bebb0c2fbd06802a8bc113d5e2eab4c87
Gabriela Pimentel <78750796+gabifp@users.noreply.github.com>
2026-02-09 15:05:25 -0300

    feat: contract pages and routing structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {ContractListComponent} from './pages/contract-list/contract-list.component';
+import {ContractUpdateComponent} from './pages/contract-update/contract-update.component';
+import {ContractViewComponent} from './pages/contract-view/contract-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'contracts', pathMatch: 'full' },
+
+  { path: 'contracts', component: ContractListComponent },
+  { path: 'contract/new', component: ContractUpdateComponent },
+  { path: 'contract/:id', component: ContractViewComponent },
+];
+
---
+++ src/app/pages/contract-view/contract-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contract-view',
+  imports: [],
+  templateUrl: './contract-view.component.html',
+  styleUrl: './contract-view.component.scss'
+})
+export class ContractViewComponent {
+
+}
