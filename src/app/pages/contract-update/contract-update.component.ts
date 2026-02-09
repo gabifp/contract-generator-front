94af6bd7ab372e07ae268dc08ef2e9c9dbfb52f3
Gabriela Pimentel <78750796+gabifp@users.noreply.github.com>
2026-02-09 15:05:25 -0300

    feat: contract pages and routing structure

---
+++ src/app/pages/contract-update/contract-update.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contract-update',
+  imports: [],
+  templateUrl: './contract-update.component.html',
+  styleUrl: './contract-update.component.scss'
+})
+export class ContractUpdateComponent {
+
+}
